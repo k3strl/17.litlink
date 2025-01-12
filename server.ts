27bf08d419a4d4daea024b2e7db4d67881004356
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-01-11 19:40:42 -0600

    added server.ts

--- server.ts
+++ server.ts
@@ -10,12 +10,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI!, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI!, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});
 
 mongoose.connection.once('open', () => {
-  console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB');
 });
 
 app.listen(PORT, () => {
-  console.log(`API server running on http://localhost:${PORT}`);
+    console.log(`API server running on http://localhost:${PORT}`);
 });
