a2f8cb7731fe6a7ce59f949c4dd70648055266af
k3strl <114842389+k3strl@users.noreply.github.com>
2025-01-14 02:24:09 -0600

    Delete config directory

--- config/connection.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-// double check this address (after the IP address)
-mongoose.connect('mongodb://127.0.0.1.27017/litlink-social-network-API');
-
-module.exports = mongoose.connection;
