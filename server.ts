18ffd44674aa27fd5d8ef9ebbcc32a493b34463e
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-01-14 21:41:05 -0600

    finished code

--- server.ts
+++ server.ts
@@ -7,6 +7,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/litlink';
 
 
 // Connect to MongoDB
